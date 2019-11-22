6f7f53a5932d65c59d7fb3705d335ab2435d1c0a
JoaoPedroPP <jp052@hotmail.com>
2019-11-22 16:27:37 -0300

    first commmit

---
+++ src/components/LogoImages/LogoImages.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TRlogo from '../../images/tr.png';
+
+const logoImages = () => (
+  <div className="logo-images">
+    <img alt="bb logo" src={TRlogo} width="100%" />
+  </div>
+);
+
+export default logoImages;
